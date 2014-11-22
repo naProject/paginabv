ef163ffae36d1bf2f93595c349a44c06a35e8fe4
jose pena toledo <josetoledo555@gmail.com>
2014-11-22 18:32:16 -0500

    Agregar plugin para crear Child-Theme, Theme Editor, imagenes  y cambio de color de fondo  para la pagina de inicio

---
+++ wordpress/wp-content/themes/evolve-Child-Theme/functions.php
@@ -0,0 +1,9 @@
+<?php /*
+
+  This file is part of a child theme called evolve Child Theme.
+  Functions in this file will be loaded before the parent theme's functions.
+  For more information, please read https://codex.wordpress.org/Child_Themes.
+
+  Add your own functions below this line.
+  ========================================== */ 
+
